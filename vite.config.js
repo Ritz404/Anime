654a300d9a41523f68a54fcc8bed67d8cd26a617
Always Ritz <161363790+Ritz404@users.noreply.github.com>
2025-01-18 15:56:32 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/;
 export default defineConfig({
   plugins: [react()],
-  server: {;
-    proxy: {;
-      '/api': {;
-        target: 'https://wan-anime-stream.vercel.app',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+  build: {
+    outDir: 'dist'
   },
+  server: {
+    port: 3000
+  }
 });
