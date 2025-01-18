83e8ca516e57183855c9770046b4dd5f2074de52
Always Ritz <161363790+Ritz404@users.noreply.github.com>
2025-01-18 15:51:51 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/;
-export default defineConfig({;
-  plugins: [react()],;
+export default defineConfig({
+  plugins: [react()],
   server: {;
     proxy: {;
       '/api': {;
-        target: 'https://cakr-anime-stream.vercel.app',;
-        changeOrigin: true,;
-        secure: false,;
-        rewrite: (path) => path.replace(/^\/api/, ''),;
-      },;
-    },;
-  },;
+        target: 'https://wan-anime-stream.vercel.app',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
